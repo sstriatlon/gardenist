00516f9da07e5439adde0f40c58d32bca14c4a79
SANTIAGO SUAREZ <sstriatlon@gmail.com>
2022-12-05 11:14:31 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo3.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>
+          Gardenist
+        </h2>
+        <h4 className="aviso">Coming soon</h4>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
